2b4d3d1ef282755904cddfcbf25aeb35cf2e58cf
Inevitable <106461411+NotAvailableRightNow@users.noreply.github.com>
2026-02-03 22:57:22 +0700

    Update QuickSort.py

--- QuickSort.py
+++ QuickSort.py
@@ -17,31 +17,3 @@ def quickSort(a, l, r):
         p = utils(a, l, r)
         quickSort(a, l, p - 1)
         quickSort(a, p + 1, r)
-        
-        
-# def quickSort(a, l, r):
-#     if l >= r:
-#         return
-#     m = (l + r) // 2
-#     a[m], a[r] = a[r], a[m]
-#     p = a[r]
-#     i = l - 1
-    
-#     for j in range(l, r):
-#         if a[j] < p:
-#             i += 1
-#             a[i], a[j] = a[j], a[i]
-            
-#     a[i + 1], a[r] = a[r], a[i + 1]
-    
-#     quickSort(a, l, i)
-#     quickSort(a, i + 2, r)
-
-# def quickSort(a):
-#     if len(a) <= 1:
-#         return a
-#     else:
-#         pivot = a[0]
-#         less = [i for i in a[1:] if i <= pivot]
-#         greater = [i for i in a[1:] if i > pivot]
-#         return quickSort(less) + [pivot] + quickSort(greater)
